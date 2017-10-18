7ec009bfa48b3dbe6b107bd5b60749ef9fd21f17
ddhuri <dhanesh@osseed.com>
2017-10-18 17:53:20 +0530

    Updated and add setting.local.php file [#151970511]

---
+++ sites/default/template.settings.local.php
@@ -0,0 +1,11 @@
+<?php
+
+$databases['default']['default'] = array(
+  'database' => '',
+  'username' => '',
+  'password' => '',
+  'host' => 'localhost',
+  'driver' => 'mysql',
+  'port' => 3306,
+  'prefix => '',
+);
